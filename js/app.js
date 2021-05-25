32729140ecc7fa7fbeb17d4f9bb033259e643ec4
Amneh-momani <Amneh99el@gmail.com>
2021-05-25 12:24:33 -0700

    update the file

--- js/app.js
+++ js/app.js
@@ -1,89 +1,69 @@
 "use strict";
 
-let sport=prompt("do you exercise evey day?");
-sport=sport.toLowerCase();
-
-let lang=prompt("Is your native language Arabic?");
-lang=lang.toLowerCase();
-
-let country=prompt("do you live in Jordan?");
-country=country.toLowerCase();
-
-let web=prompt("Are you web developer?");
-web=web.toLowerCase();
-
-let work=prompt("do you work?");
-work=work.toLowerCase();
-
 let Name=prompt("Input your name plz");
 alert("Hi "+Name+" ,welcome here.");
 
+let sport=prompt("do I exercise evey day?");
+sport=sport.toLowerCase();
 
-if(sport==="yes"){
-    console.log("your answer is yes");
-    alert("you exercise evey day");
-}else if(sport==="y"){
-    console.log("your answer is yes");
-    alert("you exercise evey day");
-}else if(sport==="no"){
-    console.log("your answer is no");
-    alert("you don't exercise evey day");
-}else if(sport==="n"){
-    console.log("your answer is no");
-    alert("you don't exercise evey day");
+if(sport==="yes" || sport==="y"){
+    alert("wrong answer because I don't exercise evey day");
+
+}else if(sport==="no" || sport==="n"){
+    alert("right, I dont't exercise evey day");
 }else{
-            console.log("you have to answer by y/n or yes/no only");
             alert("you have to answer by y/n or yes/no only");
 }
    
+
+let lang=prompt("Is my native language Arabic?");
+lang=lang.toLowerCase();
+
 switch(lang){
     case 'yes':
     case 'y':
-        console.log("your answer is yes");
-        alert("your native language is Arabic");
+        alert("right ,my native language is Arabic");
         break;
        
     case 'no':
     case 'n':
-        console.log("your answer is no");
-        alert("your native language isn't Arabic");
+        alert("wrong,my native language is Arabic");
         break;
     default:
-        console.log("you have to answer by y/n or yes/no only");
         alert("you have to answer by y/n or yes/no only");
         break;
 }
+
+let country=prompt("do I live in Jordan?");
+country=country.toLowerCase();
 
 switch(country){
     case 'yes':
     case 'y':
-        console.log("your answer is yes");
-        alert("you live in Jordan");
+        alert("yes ,I live in Jordan");
         break;
     case 'no':
     case 'n':
-        console.log("your answer is no");
-    alert("you don't live in Jordan");
+    alert("wrong answer");
         break;
     default:
-        console.log("you have to answer by y/n or yes/no only");
         alert("you have to answer by y/n or yes/no only");
         break;
 
 }
 
+let web=prompt("Am I web developer?");
+web=web.toLowerCase();
 
 switch(web){
     case 'yes':
     case 'y':
-        console.log("your answer is yes");
-        alert("you are web developer");
+        alert("you rigth, I'm web developer");
         break;
        
     case 'no':
     case 'n':
-        console.log("your answer is no");
-        alert("you aren't web developer");
+        alert("wrong answer, I'm web developer");
         break;
     default:
         console.log("you have to answer by y/n or yes/no only");
@@ -91,17 +71,18 @@ switch(web){
         break;
 }
 
+let work=prompt("do I work?");
+work=work.toLowerCase();
+
 switch(work){
     case 'yes':
     case 'y':
-        console.log("your answer is yes");
-        alert("you are working");
+        alert("No, I don't work");
         break;
        
     case 'no':
     case 'n':
-        console.log("your answer is no");
-        alert("you aren't working");
+        alert("rigth,I don't work");
         break;
     default:
         console.log("you have to answer by y/n or yes/no only");
